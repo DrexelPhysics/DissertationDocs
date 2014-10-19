3e2a173dbb946f8c284396c82eaec0b547ed31a2
Austen Groener <rocketboyausten@gmail.com>
2014-10-19 18:27:02 -0400

    Moved travis's thesis to dedicated folder. Just for aesthetics and consistency.

--- hoppe_thesis/supplement/beta_cluster_example_2/runall.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-python find_states.py
-python find_adj.py
-python calc_matrix.py .20 .5 glauber
-#python calc_matrix.py .15 .5 metro
-python grid_traj.py .95
-python kinetics.py
-python draw_clusters.py
